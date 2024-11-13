518c19a88da9e728f1711ecee0da3114779760c4
eeric <eeric.segawa@gmail.com>
2024-11-13 17:56:41 -0300

    styling footer + nav

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,20 +8,19 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='mt-auto p-8'>
-      <hr className='p-4'/>
-      <div className='flex justify-center gap-8 m-auto'>
+    <div className='mt-auto p-4 bg-[var(--gray1)]'>
+      <div className='flex justify-center gap-10 m-auto'>
         <div className=''>
-          <div><a href=""><FaLinkedinIn /></a></div>
+          <div><a href="" className="text-[var(--gray2)] hover:text-[#0A66C2]"><FaLinkedinIn className="size-6"/></a></div>
         </div>
         <div className=''>
-          <div><a href=""><ImGithub /></a></div>
+          <div><a href="" className="text-[var(--gray2)] hover:text-[#181717]"><ImGithub className="size-6"/></a></div>
         </div>
         <div className=''>
-          <div><a href=""><FaYoutube /></a></div>
+          <div><a href="" className="text-[var(--gray2)] hover:text-[#FF0000]"><FaYoutube className="size-6"/></a></div>
         </div>
       </div>
-      <div className='text-center'>
+      <div className='text-center mt-2 text-[var(--gray2)]'>
         <h4>copyright &copy; eeric</h4>
       </div>
     </div>
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -6,12 +6,12 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <div className='flex justify-between items-center mb-auto p-8'>
+    <div className='flex justify-between items-center mb-auto p-8 bg-[var(--gray2)]'>
       <Link to='/'><img src={Sun} alt="sun" className='h-10'/></Link>
       <ul className='flex gap-4'>
-        <li><Link to='/about'>About</Link></li>
-        <li><Link to='/solution'>Solution</Link></li>
-        <li><Link to='/login'>Login</Link></li>
+        <li><Link to='/about' className='btn text-[var(--gray0)] bg-[var(--green1)] hover:bg-[var(--green2)] transition-colors'>About</Link></li>
+        <li><Link to='/solution' className='btn text-[var(--gray0)] bg-[var(--blue1)] hover:bg-[var(--blue2)] transition-colors'>Solution</Link></li>
+        <li><Link to='/login' className='btn text-[var(--gray0)] bg-[var(--yellow0)] hover:bg-[var(--yellow1)] transition-colors'>Login</Link></li>
       </ul>
     </div>
   )
