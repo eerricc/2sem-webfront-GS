3563f0928029226759c72ad965bca2c322252ebc
eeric <eeric.segawa@gmail.com>
2024-11-13 10:16:01 -0300

    criando folders basicos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import { Outlet } from 'react-router-dom'
+import Nav from './components/Nav/Nav'
+import Footer from './components/Footer/Footer'
 
 function App() {
 
   return (
     <div>
+      <Nav/>
       <Outlet/>
+      <Footer/>
     </div>
   )
 }
