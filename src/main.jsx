b5760b0fa8868b3ad2be202be580cde9d2a43b12
eeric <eeric.segawa@gmail.com>
2024-11-13 10:22:52 -0300

    configurando rotas no main

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+//pages
+import Home from './routes/Home/Home.jsx'
+import About from './routes/About/About.jsx'
+import Error from './routes/Error/Error.jsx'
+import Login from './routes/Login/Login.jsx'
+import Registration from './routes/Registration/Registration.jsx'
+import Solution from './routes/Solution/Solution.jsx'
+
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 const router = createBrowserRouter([
@@ -11,7 +19,11 @@ const router = createBrowserRouter([
     errorElement:<Error/>,
 
     children:[
-      {},
+      {path:'/', element:<Home/>},
+      {path:'/about', element:<About/>},
+      {path:'/login', element:<Login/>},
+      {path:'/registration', element:<Registration/>},
+      {path:'/solution', element:<Solution/>},
     ]
   }
 ])
