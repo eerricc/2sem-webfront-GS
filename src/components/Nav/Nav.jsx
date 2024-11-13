de1b7bd9155ff42e1657414f6158d53811c88790
eeric <eeric.segawa@gmail.com>
2024-11-13 17:23:39 -0300

    nav base

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,10 +1,19 @@
+import { Link } from 'react-router-dom'
 
+import Sun from '../../assets/sun.webp'
 
 import React from 'react'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <div className='flex justify-between items-center mb-auto p-8'>
+      <Link to='/'><img src={Sun} alt="sun" className='h-10'/></Link>
+      <ul className='flex gap-4'>
+        <li><Link to='/about'>About</Link></li>
+        <li><Link to='/solution'>Solution</Link></li>
+        <li><Link to='/login'>Login</Link></li>
+      </ul>
+    </div>
   )
 }
 
